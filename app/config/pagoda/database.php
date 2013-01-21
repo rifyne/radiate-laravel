a9e5bd0c731bd10cd198cf34f4d77fcc90df9068
ChrisBorgia <cborgia@gmail.com>
2013-01-20 20:49:08 -0500

    change quote style for DB1 vars

--- app/config/pagoda/database.php
+++ app/config/pagoda/database.php
@@ -4,10 +4,10 @@ return array(
   'connections' => array(
   		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => $_SERVER['DB1_HOST'],
-			'database'  => $_SERVER['DB1_NAME'],
-			'username'  => $_SERVER['DB1_USER'],
-			'password'  => $_SERVER['DB1_PASS'],
+			'host'      => $_SERVER["DB1_HOST"],
+			'database'  => $_SERVER["DB1_NAME"],
+			'username'  => $_SERVER["DB1_USER"],
+			'password'  => $_SERVER["DB1_PASS"],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
