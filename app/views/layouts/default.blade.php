fa28ed95a7a272d662eafd19c26bf86fc0843405
cborgia <cborgia@gmail.com>
2013-04-24 10:48:29 -0400

    commented out google analytics to prevent devtools error message

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -93,10 +93,10 @@
         <script src="{{ asset('assets/scripts/js/vendor/bootstrap.min.js') }}"></script>
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
+            // var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            // (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            // g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            // s.parentNode.insertBefore(g,s)}(document,'script'));
         </script>
     </body>
 </html>
