9137763f1e2f4a79a3ced4e33f34225be4144590
Chris Borgia <chris@cborgia.me>
2015-01-28 00:52:18 -0500

    .env.example.php now in repo

---
+++ .env.example.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    // SMTP
+    'SMTP_FROM_HOST' => 'smtp.mandrillapp.com',
+    'SMTP_FROM_ADDRESS' => '',
+    'SMTP_FROM_NAME' => '',
+    'SMTP_USERNAME' => '',
+    'SMTP_PASSWORD' => '',
+
+    // DB
+    'DB_NAME' => 'radiate_www',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => '',
+];
--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(function ()
 {
-    return getenv('APP_ENV') ?: 'local';
+    return getenv('APP_ENV') ?: 'example';
 });
 
 /*
